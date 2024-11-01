62518c8aaf8684cbd7b1877290a47c2149eb7e5e
seongwoo.choi <seongwoo.choi@musinsa.com>
2024-11-02 01:57:31 +0900

    Update dry_run.go

--- cmd/dry_run.go
+++ cmd/dry_run.go
@@ -4,6 +4,7 @@ import (
 	"app/config"
 	"app/internal/node"
 	"log/slog"
+	"os"
 
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/kubernetes"
@@ -13,7 +14,7 @@ var dryRunCmd = &cobra.Command{
 	Use:   "dry-run",
 	Short: "노드 드레인 드라이런 실행",
 	Run: func(cmd *cobra.Command, args []string) {
-		kubeConfig := "local"
+		kubeConfig := os.Getenv("KUBE_CONFIG")
 		clientSet, err := config.GetKubeClientSet(kubeConfig)
 		if err != nil {
 			slog.Error("Failed to get kubernetes client", "error", err)
